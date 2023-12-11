b7d38004c1372b2380ed426547016907b550ea41
root <root@172-104-240-157.ip.linodeusercontent.com>
2023-12-11 11:12:55 +0000

    added push fn

--- lists/src/first.rs
+++ lists/src/first.rs
@@ -1,8 +1,32 @@
+use std::mem;
+
+//A mutable reference represents temporary exclusive 
+// access to a value that you don't own
+
 // pub indicates that we want to use this function outside
 // of this module
 pub struct List {
     head: Link
 }
+
+//impl => to associate actual code with a type
+// :: => name spacing operator
+impl List{
+
+    pub fn new() -> Self {
+        List { head: Link::Empty }
+    }
+
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem: elem,
+            next: mem::replace(&mut self.head, Link::Empty), 
+        });
+
+        self.head = Link::More(new_node);
+    }
+}
+
 
 enum Link {
     Empty,
