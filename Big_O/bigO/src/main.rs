67cafeadf2d06bb3b80110989c91a509ac323455
noci0001 <samuelnocita@gmail.com>
2023-12-16 11:28:37 +0100

    big_o_notation

---
+++ Big_O/bigO/src/main.rs
@@ -0,0 +1,43 @@
+
+//    __________________
+//   |                  |           EXPERIMENTAL
+//   |  BIG O NOTATION  | TWO WAYS
+//   |__________________|           THEORETICAL
+//
+// SHOWS HOW EFFICIENT AN ALGORITHM IS
+// IN THE WORST CASE SCENARIO
+//
+// IT IS BASED ON TWO FACTORS:
+//
+// (T) TIME => HOW MUCH IT TAKES TO RUN
+//
+// (N) SPACE => HOW MUCH MEMORY IT TAKES TO RUN
+//
+//
+//
+// THE FUNCTION IS -> T(N)
+//
+// T  |           x                    N is 1 (Linear expression)
+// i  |        x                       |
+// m  |     x               T(N) = a(n"1)+b
+// e  |  x                  IDEAL: LESS TIME AND LESS MEMORY
+//    |x______________      NOT IDEAL: MORE TIME AND MORE MEMORY
+//    0  1  2  3  4  5
+//       n - input size
+//
+//
+//____________________________________________________________________
+//
+//
+//
+// T  |     X                          N is 2 (quadratic expression)
+// i  |    X                           |
+// m  |   X                T(N) = a(n"2)+bn+c 
+// e  |  X                  
+//    |x______________ 
+//    0  1  2  3  4  5
+//       n - input size
+//___________________________________________________________________
+fn main() {
+    println!("Hello, world!");
+}
